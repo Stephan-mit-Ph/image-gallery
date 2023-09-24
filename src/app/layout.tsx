9cc76515323d2ec5384cfe175754b0bb17145e06
Stephan Niendorf <stephan@stephanmitph.com>
2023-09-24 06:34:50 +0200

    add image search functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,25 @@
-import "./globals.css";
-import type { Metadata } from "next";
+import './globals.css';
+import type { Metadata } from 'next';
+import Navbar from './components/Navbar';
+
+export const revalidate = 3600;
 
 export const metadata: Metadata = {
-  title: "Next.js Image Gallery",
-  description: "Next.js TypeScript Image Gallery Tutorial",
+	title: 'Next.js Image Gallery',
+	description: 'Next.js TypeScript Image Gallery Tutorial',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        <main className="max-w-6xl mx-auto">{children}</main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body>
+				<Navbar />
+				<main className='max-w-6xl mx-auto'>{children}</main>
+			</body>
+		</html>
+	);
 }
