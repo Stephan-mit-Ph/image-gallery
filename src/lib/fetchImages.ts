b33d8e53b15d9796a7a8e338544977cab605a836
Stephan Niendorf <stephan@stephanmitph.com>
2023-09-24 06:07:23 +0200

    add blurred placeholder

--- src/lib/fetchImages.ts
+++ src/lib/fetchImages.ts
@@ -1,30 +1,30 @@
-import type { ImagesResults } from "../modals/Images";
-import { ImagesSchemaWithPhotos } from "../modals/Images";
-import env from "./env";
+import type { ImagesResults } from '../models/Images';
+import { ImagesSchemaWithPhotos } from '../models/Images';
+import env from './env';
 
 export default async function fetchImages(
-  url: string
+	url: string
 ): Promise<ImagesResults | undefined> {
-  try {
-    const res = await fetch(url, {
-      headers: {
-        Authorization: env.PEXELS_API_KEY,
-      },
-    });
+	try {
+		const res = await fetch(url, {
+			headers: {
+				Authorization: env.PEXELS_API_KEY,
+			},
+		});
 
-    if (!res.ok) throw new Error("Fetch Images error!\n");
+		if (!res.ok) throw new Error('Fetch Images error!\n');
 
-    const imagesResults: ImagesResults = await res.json();
+		const imagesResults: ImagesResults = await res.json();
 
-    console.log(imagesResults);
+		console.log(imagesResults);
 
-    // Parse data with Zod schema
-    const parsedData = ImagesSchemaWithPhotos.parse(imagesResults);
+		// Parse data with Zod schema
+		const parsedData = ImagesSchemaWithPhotos.parse(imagesResults);
 
-    if (parsedData.total_results === 0) return undefined;
+		if (parsedData.total_results === 0) return undefined;
 
-    return parsedData;
-  } catch (error) {
-    if (error instanceof Error) console.error(error);
-  }
+		return parsedData;
+	} catch (error) {
+		if (error instanceof Error) console.error(error);
+	}
 }
